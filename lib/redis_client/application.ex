1aa0470d5f3f813e3a3d69b3c4d17ed9c08a2ee6
twozeronine <amdusias209@gmail.com>
2023-01-15 17:54:53 +0900

    Project init

---
+++ lib/redis_client/application.ex
@@ -0,0 +1,20 @@
+defmodule RedisClient.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: RedisClient.Worker.start_link(arg)
+      # {RedisClient.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: RedisClient.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
